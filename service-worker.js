55dc588a8022f9cf5082c57ec5e2b80c1ab628fb
Jessie-lijie <jli169869@gmail.com>
2026-07-25 13:58:56 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,73 @@
-const CACHE_NAME = "workbench-v2";
+const CACHE_NAME = "workbench-v3";
 
 const FILES = [
   "/",
   "/index.html",
   "/style.css",
-  "/app.js",
   "/manifest.json"
 ];
 
+
+// 安装
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES))
+    .then(cache => cache.addAll(FILES))
+
   );
+
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+
+
+// 激活
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys()
+    .then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
+    })
+
   );
+
+  self.clients.claim();
+
+});
+
+
+
+// 网络优先
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+    .catch(()=>{
+
+      return caches.match(event.request);
+
+    })
+
+  );
+
 });
